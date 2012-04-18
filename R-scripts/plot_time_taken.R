c11dd74675aac77ca62021447dbb3cffdbab4d12
David Laing <david@davidlaing.com>
2012-04-18 16:19:26 +0100

    A better colour palette for the graph

--- R-scripts/plot_time_taken.R
+++ R-scripts/plot_time_taken.R
@@ -1,8 +1,9 @@
 # metadata
 nodes <- list("EU-west (Ireland)"="46.137.187.155","US-west (Oregon)"="50.112.111.248","US-east(Virginia)"="23.21.131.181","US-west(North California)"="184.169.142.250","AP-south(Singapore)"="175.41.134.210","AP-North(Tokyo)"="54.248.116.221","S-America (Sao Paulo)"="177.71.182.250")
 node_ips <- unlist(nodes, use.names = FALSE)
+colours <- c("#490A3D","#BD1550","#E97F02","#F8CA00","#8A9B0F")   #From http://www.colourlovers.com/palette/848743/
 
-logdata = read.table('sample_data/IIS/rdb-srv-webl25_ex120408.log')
+logdata = read.table('sample_data/IIS/live_ex120408.log', fill=TRUE)
 
 colnames(logdata)=c('date','time','s-sitename','s-computername','s-ip','cs-method','cs-uri-stem','cs-uri-query','s-port','cs-username','c-ip','cs-version','cs(User-Agent)','cs(Cookie)','cs(Referer)','cs-host','sc-status','sc-substatus','sc-win32-status','sc-bytes','cs-bytes','time-taken')
 logdata$datetime <- strptime(paste(logdata$date,logdata$time), "%Y-%m-%d %H:%M:%S")
@@ -21,24 +22,24 @@ spreadmarkets_latency <- subset(node_latency, subset=(node_latency$"cs-uri-stem"
 spreadmarkets_latency_ts <- xts(spreadmarkets_latency$"time-taken", spreadmarkets_latency$"datetime")
 
 plot(sessionCreate_latency_ts["2012-04-08 00:00:00::2012-04-08 04:30:00"], type="n", ylim=c(0,4000), main="Comparative request latency", ylab="Latency (ms)", xlab="Timestamp (UTC)")
-points(sessionCreate_latency_ts["2012-04-08 00:00:00::2012-04-08 04:30:00"], col="black", cex=0.05)
-points(sessionDelete_latency_ts["2012-04-08 00:00:00::2012-04-08 04:30:00"], col="blue", cex=0.01)
-points(clientandtradingaccount_latency_ts["2012-04-08 00:00:00::2012-04-08 04:30:00"], col="red", cex=0.01)
-points(cfdmarkets_latency_ts["2012-04-08 00:00:00::2012-04-08 04:30:00"], col="brown", cex=0.01)
-points(spreadmarkets_latency_ts["2012-04-08 00:00:00::2012-04-08 04:30:00"], col="green", cex=0.01)
-legend(strptime("2012-04-08 03:30:00", "%Y-%m-%d %H:%M:%S"), 4000, c("Session create", "Session delete", "ClientAndTradingAccount", "CFD markets (100 mkts)", "Spread markets (0 mkts)"), pch=c(1,1,1,1,1), col=c("black", "blue", "red", "brown", "green"), cex=0.8)
+points(sessionCreate_latency_ts["2012-04-08 00:00:00::2012-04-08 04:30:00"], col=colours[1], cex=0.05)
+points(sessionDelete_latency_ts["2012-04-08 00:00:00::2012-04-08 04:30:00"], col=colours[2], cex=0.01)
+points(clientandtradingaccount_latency_ts["2012-04-08 00:00:00::2012-04-08 04:30:00"], col=colours[3], cex=0.01)
+points(cfdmarkets_latency_ts["2012-04-08 00:00:00::2012-04-08 04:30:00"], col=colours[4], cex=0.01)
+points(spreadmarkets_latency_ts["2012-04-08 00:00:00::2012-04-08 04:30:00"], col=colours[5], cex=0.01)
+legend(strptime("2012-04-08 03:30:00", "%Y-%m-%d %H:%M:%S"), 4000, c("Session create", "Session delete", "ClientAndTradingAccount", "CFD markets (100 mkts)", "Spread markets (0 mkts)"), pch=c(1,1,1,1,1), col=colours, cex=0.8)
 
 plot(sessionCreate_latency_ts["2012-04-08 01:30:00::"], type="n", ylim=c(0,1000), main="Session create request latency", ylab="Latency (ms)", xlab="Timestamp (UTC)")
-points(sessionCreate_latency_ts["2012-04-08 01:30:00::"], col="black", cex=0.01)
+points(sessionCreate_latency_ts["2012-04-08 01:30:00::"], col=colours[1], cex=0.01)
 
 plot(sessionDelete_latency_ts, type="n", ylim=c(0,300), main="Session delete request latency", ylab="Latency (ms)", xlab="Timestamp (UTC)")
-points(sessionDelete_latency_ts, col="red", cex=0.01)
+points(sessionDelete_latency_ts, col=colours[2], cex=0.01)
 
 plot(clientandtradingaccount_latency_ts, type="n", ylim=c(0,300), main="ClientAndTradingAccount request latency", ylab="Latency (ms)", xlab="Timestamp (UTC)")
-points(clientandtradingaccount_latency_ts, col="blue", cex=0.01)
+points(clientandtradingaccount_latency_ts, col=colours[3], cex=0.01)
 
 plot(cfdmarkets_latency_ts, type="n", ylim=c(0,300), main="CFD markets request latency ( 100 markets )", ylab="Latency (ms)", xlab="Timestamp (UTC)")
-points(cfdmarkets_latency_ts, col="brown", cex=0.01)
+points(cfdmarkets_latency_ts, col=colours[4], cex=0.01)
 
 plot(spreadmarkets_latency_ts, type="n", ylim=c(0,1000), main="Spread markets request latency ( 0 markets )", ylab="Latency (ms)", xlab="Timestamp (UTC)")
-points(spreadmarkets_latency_ts, col="green", cex=0.01)
+points(spreadmarkets_latency_ts, col=colours[5], cex=0.01)

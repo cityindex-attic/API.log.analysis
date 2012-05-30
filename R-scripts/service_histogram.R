cf55a28df74f60db2cc70a7b3fd3095c093a08aa
David Laing <mrdavidlaing@ip-10-244-10-54.eu-west-1.compute.internal>
2012-05-30 17:04:23 +0000

    new scripts

---
+++ R-scripts/service_histogram.R
@@ -0,0 +1,12 @@
+library("ggplot2")
+
+logdata = read.table('/mnt/ciapi.cityindex.com/2012-05/session.log', fill=TRUE)
+colnames(logdata)=c('date','time','s-sitename','scomputername','s-ip','cs-method','cs-uri-stem','cs-uri-query','s-port','cs-username','c-ip','cs-version','cs(User-Agent)','cs(Cookie)','cs(Referer)','cs-host','sc-status','sc-substatus','sc-win32-status','sc-bytes','cs-bytes','timetaken')
+logdata$datetime <- strptime(paste(logdata$date,logdata$time), "%Y-%m-%d %H:%M:%S")
+
+d <- ggplot(logdata, aes(x=datetime, y=timetaken))
+d <- d + geom_hex(bins=100)
+d <- d + ylim(0,1000)
+d <- d + scale_fill_gradient(low="white", high="red")
+d <- d + facet_grid(scomputername ~ .)
+print(d)
